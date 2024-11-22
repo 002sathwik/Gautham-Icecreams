d36d5bc29aa2f8f62a1f1ae735f302e20e70e603
Sathwik <hsathwik002@gmail.com>
2024-11-22 11:50:06 +0530

    fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,8 +13,7 @@ export default function HeroSection() {
 
 
       <video
-        id="loopVideo"
-        src={"/v3.mp4"}
+        src={"/v4.mp4"}
         className="absolute top-0 left-0 w-full h-full object-cover z-10 -mt-14"
         autoPlay
         muted={!muted}
